88190df5952cd5429c9fb353920cccef7462624e
Derek Parker and Jack Christensen <dev+dparker1990+jackc@hashrocket.com>
2013-11-13 13:48:56 -0600

    Refactor ServeHTTP

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
+	"log"
 	"net/http"
 )
 
@@ -19,23 +19,33 @@ type Stub struct {
 
 func (h *StubHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	if req.URL.Path == "/stub" && req.Method == "POST" {
-		var stub Stub
-		jsonDecoder := json.NewDecoder(req.Body)
-		err := jsonDecoder.Decode(&stub)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-		}
-
-		h.stubs[stub.Path] = stub
+		h.createStubHandler(w, req)
 	} else {
-		if stub, ok := h.stubs[req.URL.Path]; ok {
-			if stub.Method == req.Method {
-				// TODO - handle errors
-				_, _ = io.WriteString(w, stub.Body)
+		h.returnStubHandler(w, req)
+	}
+}
+
+func (h *StubHandler) createStubHandler(w http.ResponseWriter, req *http.Request) {
+	var stub Stub
+	jsonDecoder := json.NewDecoder(req.Body)
+	err := jsonDecoder.Decode(&stub)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	h.stubs[stub.Path] = stub
+}
+
+func (h *StubHandler) returnStubHandler(w http.ResponseWriter, req *http.Request) {
+	if stub, ok := h.stubs[req.URL.Path]; ok {
+		if stub.Method == req.Method {
+			_, err := io.WriteString(w, stub.Body)
+			if err != nil {
+				log.Printf("Could not send response to client due to: %v", err)
 			}
-		} else {
-			w.WriteHeader(http.StatusNotFound)
 		}
+	} else {
+		w.WriteHeader(http.StatusNotFound)
 	}
 }
 
@@ -46,5 +56,4 @@ func NewStubHandler() *StubHandler {
 }
 
 func main() {
-	fmt.Println("Hello, world!")
 }

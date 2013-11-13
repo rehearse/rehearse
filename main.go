e1cc335b3a3d56737f19c6ba01138e2599e81638
Derek Parker and Jack Christensen <dev+dparker1990+jackc@hashrocket.com>
2013-11-13 16:59:13 -0600

    Add main and make it runnable

--- main.go
+++ main.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"net/http"
+)
+
+var config struct {
+	path    string
+	address string
+	port    int
+}
+
 func main() {
+	flag.StringVar(&config.path, "path", "", "Optional path to serve static files from.")
+	flag.StringVar(&config.address, "address", "127.0.0.1", "Address to listen on.")
+	flag.IntVar(&config.port, "port", 3333, "Port to listen on.")
+	flag.Parse()
+
+	stubHandler := NewStubHandler()
+	if config.path != "" {
+		stubHandler.fallbackHandler = http.FileServer(http.Dir(config.path))
+	}
+	http.Handle("/", stubHandler)
+	http.ListenAndServe(fmt.Sprintf("%s:%d", config.address, config.port), stubHandler)
 }

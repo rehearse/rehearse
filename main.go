63f88cfadaad4813b891143ad0e861d25dd32d04
Derek Parker and Jack Christensen <dev+dparker1990+jackc@hashrocket.com>
2013-11-15 11:19:29 -0600

    Add version

--- main.go
+++ main.go
@@ -5,19 +5,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+)
+
+const (
+	VERSION = "0.1"
 )
 
 var config struct {
-	path    string
-	address string
-	port    int
+	path         string
+	address      string
+	port         int
+	printVersion bool
 }
 
 func main() {
 	flag.StringVar(&config.path, "path", "", "Optional path to serve static files from.")
 	flag.StringVar(&config.address, "address", "127.0.0.1", "Address to listen on.")
 	flag.IntVar(&config.port, "port", 3333, "Port to listen on.")
+	flag.BoolVar(&config.printVersion, "version", false, "Print version and exit.")
 	flag.Parse()
+
+	if config.printVersion {
+		fmt.Printf("rehearse v%s\n", VERSION)
+		os.Exit(0)
+	}
 
 	stubHandler := NewStubHandler()
 	if config.path != "" {

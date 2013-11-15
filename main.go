3547204bfd5f831b881bfca4f28d9704f305b1a8
Derek Parker and Jack Christensen <dev+dparker1990+jackc@hashrocket.com>
2013-11-15 11:20:53 -0600

    Rename config to options

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ const (
 	VERSION = "0.1"
 )
 
-var config struct {
+var options struct {
 	path         string
 	address      string
 	port         int
@@ -20,25 +20,25 @@ var config struct {
 }
 
 func main() {
-	flag.StringVar(&config.path, "path", "", "Optional path to serve static files from.")
-	flag.StringVar(&config.address, "address", "127.0.0.1", "Address to listen on.")
-	flag.IntVar(&config.port, "port", 3333, "Port to listen on.")
-	flag.BoolVar(&config.printVersion, "version", false, "Print version and exit.")
+	flag.StringVar(&options.path, "path", "", "Optional path to serve static files from.")
+	flag.StringVar(&options.address, "address", "127.0.0.1", "Address to listen on.")
+	flag.IntVar(&options.port, "port", 3333, "Port to listen on.")
+	flag.BoolVar(&options.printVersion, "version", false, "Print version and exit.")
 	flag.Parse()
 
-	if config.printVersion {
+	if options.printVersion {
 		fmt.Printf("rehearse v%s\n", VERSION)
 		os.Exit(0)
 	}
 
 	stubHandler := NewStubHandler()
-	if config.path != "" {
-		stubHandler.fallbackHandler = http.FileServer(http.Dir(config.path))
+	if options.path != "" {
+		stubHandler.fallbackHandler = http.FileServer(http.Dir(options.path))
 	}
 	http.Handle("/", stubHandler)
 
 	var err error
-	err = http.ListenAndServe(fmt.Sprintf("%s:%d", config.address, config.port), stubHandler)
+	err = http.ListenAndServe(fmt.Sprintf("%s:%d", options.address, options.port), stubHandler)
 	if err != nil {
 		log.Fatalf("Unable to start server: %v\n", err)
 	}
